b2749bb437e46a23d05876527ebd81e294dd8578
Neil <neil@Neils-MacBook-Pro.local>
2015-06-16 19:13:46 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Priceline-Junit-API-Test
+Will run against priceline autosuggest API to see when "San" is typed, then "San Francisco" should be the first suggestion.
+
+# Running the tests
+to run: `mvn test`
---
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -0,0 +1,37 @@
+package com.yourcompany;
+
+import org.json.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+import java.io.IOException;
+import static org.junit.Assert.*;
+
+
+/**
+ * Demonstrates how to write a JUnit test that test an API
+ *
+ * @author Neil Manvar
+ */
+public class SampleSauceTest {
+    
+    @Test
+    public void testGithubUsersNdmanvar()
+          throws ClientProtocolException, IOException, JSONException{
+       HttpUriRequest request = new HttpGet( "http://www.priceline.com/svcs/ac/index/hotels/san/6/3/3/3" );
+       
+       HttpResponse httpResponse =	 HttpClientBuilder.create().build().execute( request );
+       
+       String bodyAsString = EntityUtils.toString(httpResponse.getEntity());
+       
+       JSONObject obj = new JSONObject(bodyAsString);
+       
+       String itemName = obj.getJSONArray("searchItems").getJSONObject(0).getString("itemName");
+       
+       assertEquals("San Francisco, CA", itemName);
+    }
+}
